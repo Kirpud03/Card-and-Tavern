e6301035599a42f37526460f25cbc174bc8d9722
Kirpud03 <k.pivnichenko@mail.ru>
2024-05-29 16:29:50 +0300

    Menu

--- Cards and Taverns/Assets/Scripts/GameManager.cs
+++ Cards and Taverns/Assets/Scripts/GameManager.cs
@@ -1,12 +1,22 @@
 using UnityEngine;
 using Cinemachine;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public CinemachineVirtualCamera cam1;
     public CinemachineVirtualCamera cam2;
     public List<GameObject> cardZone;
+    private bool _playerLose;
+    private bool _playerWin;
+    private bool _pause;
+    [SerializeField]
+    private GameObject _pauseMenu, _loseMenu, _winMenu;
+    private void Start()
+    {
+        Time.timeScale = 1;
+    }
     private void Update()
     {
         StaticHolder.cardTable = GameObject.FindGameObjectsWithTag("CardPuted");
@@ -37,5 +47,55 @@ public class GameManager : MonoBehaviour
         {
             StaticHolder.playerTurn = false;
         }
+        if (StaticHolder.plHealth <= 0)
+        {
+            _playerLose = true;
+        }
+        if(StaticHolder.enHealth <= 0)
+        {
+            _playerWin = true;
+        }
+        if (_playerWin)
+        {
+            Time.timeScale = 0;
+            _winMenu.SetActive(true);
+        }
+        if (_playerLose)
+        {
+            Time.timeScale = 0;
+            _loseMenu.SetActive(true);
+        }
+        if (_pause)
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+    }
+    public void PauseMenu()
+    {
+        _pause = !_pause;
+        _pauseMenu.SetActive(_pause);
+    }
+    public void ResumeButton()
+    {
+        _pause = false;
+        _pauseMenu.SetActive(_pause);
+    }
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+    public void MenuButton()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(0);
+    }
+    public void RestartButton()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(1);
     }
 }
